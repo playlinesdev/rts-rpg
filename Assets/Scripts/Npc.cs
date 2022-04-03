487319b7a9d1e1d760e96605b6b1eb736bef4db5
Bruno Batista Rafante <rafante2@gmail.com>
2022-04-03 10:49:23 -0300

    npc class

---
+++ Assets/Scripts/Npc.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+namespace Logic
+{
+    [RequireComponent(typeof(NavMeshAgent))]
+    public class Npc : MonoBehaviour
+    {
+        [SerializeField] private NavMeshAgent agent;
+        [SerializeField] private Transform destination;
+
+        private void OnValidate()
+        {
+            if (agent == null)
+                agent = GetComponent<NavMeshAgent>();
+        }
+
+        private void FixedUpdate()
+        {
+
+            if (destination != null)
+                agent.destination = destination.position;
+        }
+    }
+}
